0e8d8a8086e6dde4aa022b9cbddb188bbfddd503
Abhyuday Singh <47112147+abhyuday2002@users.noreply.github.com>
2024-10-21 22:56:35 -0700

    Added Files for Calendar Screen

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import CalendarScreen from './screens/CalendarScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#FF6B6B',
+          },
+          headerTintColor: '#fff',
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Calendar" component={CalendarScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
