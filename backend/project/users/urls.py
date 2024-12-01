127b64469f04a670c08383cce04e3172448003d2
Lucas Tuan <lucastuan@gmail.com>
2024-11-30 21:15:48 -0800

    Added a way to see profiles of users and stuff for the backend, and an example website thing

--- backend/project/users/urls.py
+++ backend/project/users/urls.py
@@ -4,11 +4,12 @@ from users import views
 
 urlpatterns = [
     path('', views.api_root),
-    path('things/', views.ThingList.as_view(), name='thing-list'),
-    path('things/<int:pk>/', views.ThingDetail.as_view(), name='thing-detail'),
+    path('profiles/', views.ProfileList.as_view(), name='profiles-list'),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view(), name='profile-detail'),
     path('users/', views.UserList.as_view(), name="user-list"),
     path('users/<int:pk>/', views.UserDetail.as_view(), name="user-detail"),
-    path('users/create', views.UserAdd.as_view(), name="user-add")
+    path('users/create', views.UserAdd.as_view(), name="user-add"),
+    path('login/', views.SingleProfile.as_view(), name="single-profile"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
